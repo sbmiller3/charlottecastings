f765284474dd1bf1414854e4a70a5eeb2a7c915b
sbmiller3 <sbmiller3@yahoo.com>
2017-09-19 07:32:26 -0400

    working on it

--- trial.php
+++ trial.php
@@ -1,12 +1,17 @@
+</<?php
+  $title="my site";
+  $description="this is ti";
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title><?php echo $title; ?></title>
 </head>
 <body>
-
+  <h1><?php echo $title; ?></h1>
 </body>
 </html>
