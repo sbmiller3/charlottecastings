30ef3633d9ff2dd58893676aedd9685c7222b978
sbmiller3 <sbmiller3@yahoo.com>
2018-03-27 13:55:49 -0400

    still early

--- trial.php
+++ trial.php
@@ -1,18 +1,49 @@
-</<?php
+<?php
   $title="my site";
-  $description="this is ti";
+  $description="this is it";
+  $style="./css/style2.css";
+  
+  require("./inc/dtype_head_remains_open.php");
  ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title><?php echo $title; ?></title>
+
 </head>
 <body>
+<div class="page-container">
+<nav id="nav_topline">
+	<span> <?php echo $title; ?></span>
+	<ul>
+		<li>w</li>
+		<li>x</li>
+		<li>y</li>
+		<li>z</li>
+	</ul>
+  <form  >
+  <input type="text" />
+  <input type="submit"  value="Search this site"  />
+  </form>
+</nav>
+<header>
   <h1><?php echo $title; ?></h1>
   <h3><?php echo $description; ?></h3>
+</header>
+  
+  
+ 
+</div> <!-- /.page-container -->
+   
+<main>
+<aside>sidebar</aside>
+<section>
+<article>content</article>
+</section>
+</main>  
+ 
+<div class="page-container"> 
+  <footer>
+  footer
+  </footer>  
+</div> <!-- /.page-container -->
+  
 </body>
 </html>
